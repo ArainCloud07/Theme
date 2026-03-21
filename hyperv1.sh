bc20633c72a26a1c977fcdbe521276144ad110a0
Sourin De <sdgamer8263@gmail.com>
2026-03-22 00:02:10 +0530

    Update hyperv1.sh

--- hyperv1.sh
+++ hyperv1.sh
@@ -1,4 +1,8 @@
-sudo dpkg --configure -a
-wget -O installer.sh https://r2.rolexdev.tech/hyperv1/installer.sh
-chmod +x installer.sh
-sudo ./installer.sh
+   wget -O installer.sh https://r2.rolexdev.tech/hyperv1/installer.sh
+            chmod +x installer.sh
+            sudo ./installer.sh
+            rm installer.sh
+            cd /var/www/pterodactyl
+            php artisan view:clear
+            php artisan config:clear
+            php artisan queue:restart

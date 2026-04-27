a28873f9f293cd604cc5668ea42d34445edde7e6
Sourin De <sdgamer8263@gmail.com>
2026-04-27 21:07:41 +0530

    Update V

--- V.s
+++ V.s
@@ -1,6 +1,11 @@
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && \
+apt-get install -y nodejs && \
+npm install -g yarn && \
 cd /var/www/pterodactyl && \
-wget https://raw.githubusercontent.com/sdgamer8263-sketch/Theme/main/F/sdsa.zip && \
+wget -O sdsa.zip https://raw.githubusercontent.com/sdgamer8263-sketch/Theme/main/F/sdsa.zip && \
 unzip -o sdsa.zip && \
+cp -r sdsa/* ./ && \
+rm -rf sdsa sdsa.zip && \
 yarn install && \
 yarn build:production && \
 php artisan view:clear && \
